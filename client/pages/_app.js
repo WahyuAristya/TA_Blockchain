043be545ee480849e89f6456b533367bdf3c9d8c
Wahyu Aristya <wahyuaristya08@gmail.com>
2024-04-03 19:47:46 +0800

    ganti wallet lama

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,107 +1,3 @@
-// import { useEffect, useState } from 'react';
-// import '../styles/globals.css';
-// import 'react-toastify/dist/ReactToastify.css';
-// import { ToastContainer } from 'react-toastify';
-// import { wrapper } from '../redux/store';
-// import { useDispatch } from 'react-redux';
-// import { getAllFunding, loadAccount, loadCrowdFundingContract, loadWeb3 } from '../redux/interactions';
-// import { Router } from 'next/router';
-// import NProgress from 'nprogress';
-// import "nprogress/nprogress.css";
-// import { chainOrAccountChangedHandler } from '../helper/helper';
-
-// function MyApp({ Component, pageProps }) {
-//   const dispatch = useDispatch();
-//   const [isWalletConnected, setIsWalletConnected] = useState(false);
-//   const [hasMetaMask, setHasMetaMask] = useState(false);
-
-//   useEffect(() => {
-//     loadBlockchain();
-//     checkForMetaMask();
-//   }, []);
-
-//   const loadBlockchain = async () => {
-//     const web3 = await loadWeb3(dispatch);
-//     const account = await loadAccount(web3, dispatch);
-//     // const crowdFundingContract = await loadCrowdFundingContract(web3, dispatch);
-//     // await getAllFunding(crowdFundingContract, web3, dispatch);
-//   }
-
-//   const checkForMetaMask = () => {
-//     if (window.ethereum) {
-//       setHasMetaMask(true);
-//     }
-//   }
-
-//   const connectWallet = async () => {
-//     try {
-//       if (window.ethereum) {
-//         await window.ethereum.request({ method: 'eth_requestAccounts' });
-//         const web3 = await loadWeb3(dispatch);
-//         const account = await loadAccount(web3, dispatch);
-//         setIsWalletConnected(true);
-//         registerMetaMaskEvents();
-//         const crowdFundingContract = await loadCrowdFundingContract(web3, dispatch);
-//         await getAllFunding(crowdFundingContract, web3, dispatch);
-//       } else {
-//         console.error('MetaMask extension not detected.');
-//       }
-//     } catch (error) {
-//       console.error('Failed to connect to wallet:', error);
-//     }
-//   };
-
-//   const registerMetaMaskEvents = async () => {
-//     try {
-//       window.ethereum.on('accountsChanged', chainOrAccountChangedHandler);
-//       window.ethereum.on('chainChanged', chainOrAccountChangedHandler);
-//     } catch (error) {
-//       console.error('Failed to register MetaMask events:', error);
-//     }
-//   };
-
-//   Router.events.on("routeChangeStart", () => NProgress.start());
-//   Router.events.on("routeChangeComplete", () => NProgress.done());
-//   Router.events.on("routeChangeError", () => NProgress.done());
-
-//   return (
-//     <>
-//       <ToastContainer />
-//       {isWalletConnected ? (
-//         <Component {...pageProps} />
-//       ) : (
-//         <>
-//           {hasMetaMask ? (
-//             <div className="flex flex-col items-center justify-center my-40">
-//               <p>Connect your wallet to proceed.</p>
-//               <button onClick={connectWallet} className="p-4 my-10 text-lg font-bold text-white rounded-md w-56 bg-[#4CAF50] drop-shadow-md hover:bg-[#357a38] hover:drop-shadow-xl">
-//                 Connect Wallet
-//               </button>
-//             </div>
-//           ) : (
-//             <div className="flex flex-col items-center justify-center my-40">
-//               <p>MetaMask is not detected. Please install MetaMask to use this website.</p>
-//               <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer">
-//                 <button className="p-4 my-10 text-lg font-bold text-white rounded-md w-56 bg-[#8D8DAA] drop-shadow-md hover:bg-[#b1b1d6] hover:drop-shadow-xl">
-//                   Download MetaMask
-//                 </button>
-//               </a>
-//             </div>
-//           )}
-//         </>
-//       )}
-//     </>
-//   );
-// }
-
-// export default wrapper.withRedux(MyApp);
-
-
-
-// "react": "17.0.2",
-
-
-
 import { useEffect, useState } from 'react';
 import '../styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -116,14 +12,13 @@ import { chainOrAccountChangedHandler } from '../helper/helper';
 
 
 import {
-  useConnectionStatus,
-  useDisconnect,
   ThirdwebProvider,
   ConnectWallet,
   metamaskWallet,
   coinbaseWallet,
   walletConnect,
-  trustWallet
+  zerionWallet,
+  darkTheme
 } from "@thirdweb-dev/react";
 
 function MyApp({ Component, pageProps }) {
@@ -152,7 +47,7 @@ function MyApp({ Component, pageProps }) {
         const crowdFundingContract = await loadCrowdFundingContract(web3, dispatch);
         await getAllFunding(crowdFundingContract, web3, dispatch);
       } else {
-        console.error('MetaMask extension not detected.');
+        console.error('Wallet not detected.');
       }
     } catch (error) {
       console.error('Failed to connect to wallet:', error);
@@ -181,6 +76,7 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       ) : (
         <div className="flex flex-col items-center justify-center my-40">
+          <h1 className='font-mono text-3xl text-greay font-bold hidden lg:block'>CROWDFUNDING DONATION</h1>
           <div className="flex flex-col items-center justify-center my-40">
             <ThirdwebProvider
               activeChain="localhost"
@@ -188,14 +84,19 @@ function MyApp({ Component, pageProps }) {
               autoConnect={false}
               // locale={en()}
               supportedWallets={[
-                metamaskWallet(),
-                coinbaseWallet({ recommended: true }),
+                metamaskWallet({ recommended: true }),
+                coinbaseWallet(),
                 walletConnect(),
-                trustWallet(),
+                zerionWallet(),
               ]}
             >
               <ConnectWallet
-                theme={"dark"}
+                theme={darkTheme({
+                  colors:{
+                    primaryButtonBg: "#4CAF50",
+                    primaryButtonText: "#ededef",
+                  }
+                })}
                 modalTitle={"Choose Your Wallet"}
                 modalSize={"compact"}
                 welcomeScreen={{
@@ -204,11 +105,13 @@ function MyApp({ Component, pageProps }) {
                 }}
                 showThirdwebBranding={false}
                 onConnect={connectWallet}
-                switchToActiveChain={true}
+                autoConnect={false}
+                switchToActiveChain={false}
+                style={{ fontSize: "1.5rem", padding: "0.9rem 1.7rem" }}
               />
+            <p style={{ marginTop: "1rem" }}>Connect your wallet to proceed.</p>
             </ThirdwebProvider>
           </div>
-          <p>Connect your wallet to proceed.</p>
         </div>
       )}
     </>
